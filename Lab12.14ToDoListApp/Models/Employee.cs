08ae82173e7acd5aad329ec53bcef2b262c65dc0
Andy Beer <AndrewBeer@RocketMortgage.com>
2021-12-14 14:59:32 -0500

    First Commit - model wireframes

---
+++ Lab12.14ToDoListApp/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12._14ToDoListApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Range (0,40)]
+        public int Hours { get; set; }
+        public string Title { get; set; }
+        public List<ToDo> ToDoList { get; set; }
+
+    }
+}
---
+++ Lab12.14ToDoListApp/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12._14ToDoListApp.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int AssignedTo { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
